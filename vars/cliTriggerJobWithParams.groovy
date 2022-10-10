3136cdf2a37d011f1b949a7b26861296678cb0ef
tsingkofer-cb <tsingkofer@cloudbees.com>
2022-10-10 14:46:27 -0500

    Update cliTriggerJobWithParams.groovy

--- vars/cliTriggerJobWithParams.groovy
+++ vars/cliTriggerJobWithParams.groovy
@@ -7,6 +7,6 @@ def call(String fileName, String controllerUrl = '', String jobPath = '', String
 
     //Trigger build
     withCredentials([usernamePassword(credentialsId: 'oc-api-token', passwordVariable: 'JENKINS_API_TOKEN', usernameVariable: 'JENKINS_USER_ID')]) {
-        sh "java -jar jenkins-cli.jar -s $controllerUrl -webSocket build $jobPath -f -p TF_VARS_PARAM="""$DEMO_TF_VARS""" "
+        sh """java -jar jenkins-cli.jar -s $controllerUrl -webSocket build $jobPath -f -p TF_VARS_PARAM="$DEMO_TF_VARS" """
     }
 }
