017a750e32051ecab57ca140c479242ee82bdf7b
tsingkofer-cb <tsingkofer@cloudbees.com>
2022-10-10 14:51:18 -0500

    switching to map

--- vars/cliTriggerJobWithParams.groovy
+++ vars/cliTriggerJobWithParams.groovy
@@ -1,12 +1,15 @@
-def call(String fileName = '', String controllerUrl = '', String jobPath = '', String waitForResult = 'true'){
+def call(Map config){
+
+    //String fileName = '', String controllerUrl = '', String jobPath = '', String waitForResult = 'true'
+
     //Get CLI jar
     sh 'curl -O $JENKINS_URL/jnlpJars/jenkins-cli.jar'
 
     //Construct params
-    def DEMO_TF_VARS = readFile "$fileName"
+    def DEMO_TF_VARS = readFile "$config.fileName"
 
     //Trigger build
     withCredentials([usernamePassword(credentialsId: 'oc-api-token', passwordVariable: 'JENKINS_API_TOKEN', usernameVariable: 'JENKINS_USER_ID')]) {
-        sh """java -jar jenkins-cli.jar -s $controllerUrl -webSocket build $jobPath -f -p TF_VARS_PARAM="$DEMO_TF_VARS" """
+        sh """java -jar jenkins-cli.jar -s $config.controllerUrl -webSocket build $config.jobPath -f -p TF_VARS_PARAM="$DEMO_TF_VARS" """
     }
 }
