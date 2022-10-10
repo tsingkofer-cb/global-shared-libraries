2300ac7d038d0db56bd653b9452285d9a37143e4
tsingkofer-cb <tsingkofer@cloudbees.com>
2022-10-10 15:11:00 -0500

    adding additional param handling

--- vars/cliTriggerJobWithParams.groovy
+++ vars/cliTriggerJobWithParams.groovy
@@ -1,15 +1,19 @@
 def call(Map config){
 
-    //String fileName = '', String controllerUrl = '', String jobPath = '', String waitForResult = 'true'
-
     //Get CLI jar
     sh 'curl -O $JENKINS_URL/jnlpJars/jenkins-cli.jar'
 
     //Construct params
     def DEMO_TF_VARS = readFile "$config.fileName"
+    def PARAMETER_STRING = ''
+    for (key in config.keySet()){
+        if (key.startsWith("PARAM")){
+            PARAMETER_STRING = $PARAMETER_STRING + ' -p ' + key + '=' + config.get(key)
+        }
+    }
 
     //Trigger build
     withCredentials([usernamePassword(credentialsId: 'oc-api-token', passwordVariable: 'JENKINS_API_TOKEN', usernameVariable: 'JENKINS_USER_ID')]) {
-        sh """java -jar jenkins-cli.jar -s $config.controllerUrl -webSocket build $config.jobPath -f -p TF_VARS_PARAM="$DEMO_TF_VARS" """
+        sh """java -jar jenkins-cli.jar -s $config.controllerUrl -webSocket build $config.jobPath -f $PARAMETER_STRING -p TF_VARS_PARAM="$DEMO_TF_VARS" """
     }
 }
