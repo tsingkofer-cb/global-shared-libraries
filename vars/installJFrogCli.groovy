263d7d23eaa4cbf932c115d0c3d52469fea7ddf3
tsingkofer-cb <tsingkofer@cloudbees.com>
2024-11-19 10:44:19 -0600

    Update installJFrogCli.groovy

--- vars/installJFrogCli.groovy
+++ vars/installJFrogCli.groovy
@@ -1,32 +1,37 @@
 def call(Map config){
     // withCredentials([usernamePassword(credentialsId: '', passwordVariable: 'ARTIFACTORY_PSW', usernameVariable: 'ARTIFACTORY_USR')]){
-        sh '''#!/bin/bash
-        function ver { printf "%03d%03d%03d%03d" $(echo "$1" | tr '.' ' '); }
+        if (isUnix()){
+            sh '''#!/bin/bash
+            function ver { printf "%03d%03d%03d%03d" $(echo "$1" | tr '.' ' '); }
 
-        if ! command -v jf 2>&1 >/dev/null 
-        then
-            echo "JFrog CLI not found. Proceeding with installation.";
-            # get install script from Artifactory...
-            # curl -u $ARTIFACTORY_USR:$ARTIFACTORY_PSW https://artifactory/path/to/install/script/install-cli.sh -o install-cli.sh
-
-            # get from JFrog site directly...
-            curl -fL https://install-cli.jfrog.io -o install-cli.sh
-
-            #install a specific version of the CLI
-            sh install-cli.sh 2.71.3
-        else
-            echo "JFrog CLI already installed."
-            jf --version
-            INSTALLED_VERSION=$(jf --version | cut -c11-)
-            if [ $(ver ${INSTALLED_VERSION}) -lt $(ver 2.71.3) ]
+            if ! command -v jf 2>&1 >/dev/null 
             then
-                echo "Newer approved version available."
-                #install the latest version of the CLI
+                echo "JFrog CLI not found. Proceeding with installation.";
+                # get install script from Artifactory...
+                # curl -u $ARTIFACTORY_USR:$ARTIFACTORY_PSW https://artifactory/path/to/install/script/install-cli.sh -o install-cli.sh
+
+                # get from JFrog site directly...
+                curl -sfL https://install-cli.jfrog.io -o install-cli.sh
+
+                #install a specific version of the CLI
                 sh install-cli.sh 2.71.3
             else
-                echo "This is the latest version."
+                echo "JFrog CLI already installed."
+                jf --version
+                INSTALLED_VERSION=$(jf --version | cut -c11-)
+                if [ $(ver ${INSTALLED_VERSION}) -lt $(ver 2.71.3) ]
+                then
+                    echo "Newer approved version available."
+                    #install the latest version of the CLI
+                    sh install-cli.sh 2.71.3
+                else
+                    echo "This is the latest version."
+                fi
             fi
-        fi
-        '''
+            '''
+        } else { //for windows agents
+            // powershell
+            // bat
+        }
     // }
 }
